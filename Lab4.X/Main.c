a6d8ac65e5126f4c2ddb2e6c8af0275647fc76c5
Tristen <trkmille@ucsc.edu>
2024-02-27 13:01:18 -0800

    All three Euler Angles seem to be correct

--- Lab4.X/Main.c
+++ Lab4.X/Main.c
@@ -34,25 +34,27 @@ float getTheta(Matrix3x3 *matrix)
 {
     float raw = getEntry(matrix, 0, 2);
     float rads = -asin(raw);
-    return convertRadToDeg(rads);
+    return rads;
 }
 
 float getPsi(Matrix3x3 *matrix)
 {
     float theta = getTheta(matrix);
-    float raw = getEntry(matrix, 0, 0);
+    float raw = getEntry(matrix, 0, 1);
+    printf("Raw psi = %f\n", raw);
     raw /= cos(theta);
-    float rads = acos(raw);
-    return convertRadToDeg(rads);
+    printf("Psi after division by cos(theta): %f\n", raw);
+    float rads = asin(raw);
+    return (rads);
 }
 
 float getPhi(Matrix3x3 *matrix)
 {
     float theta = getTheta(matrix);
-    float raw = getEntry(matrix, 2, 2);
+    float raw = getEntry(matrix, 1, 2);
     raw /= cos(theta);
-    float rads = acos(raw);
-    return convertRadToDeg(rads);
+    float rads = asin(raw);
+    return (rads);
 }
 
 int main(void)
@@ -62,11 +64,30 @@ int main(void)
         {
             {0.8293, 0.5498, -0.0998},
             {-0.5581, 0.8059, -0.1977},
-            {-0.0282, 0.2197, 0.9752}}
-    } ;
+            {-0.0282, 0.2197, 0.9752}
+        }
+    };
     float theta = getTheta(&foo);
+    theta = convertRadToDeg(theta);
     float psi = getPsi(&foo);
+    psi = convertRadToDeg(psi);
     float phi = getPhi(&foo);
-    printf("Theta:%f  Psi:%f  Phi:%f\n", theta, psi, phi);
+    phi = convertRadToDeg(phi);
+    printf("Theta:%f  Psi:%f  Phi:%f\n", theta, psi, phi); // Expected values: Theta:5.727654  Psi:33.544071  Phi:-11.460480
+    Matrix3x3 foo2 = {
+        {
+            {0.8293, -0.5498, 0.0998},
+            {0.5581, 0.8059, -0.1977},
+            {0.0282, 0.2197, 0.9752}
+        }
+    };
+    theta = getTheta(&foo2);
+    theta = convertRadToDeg(theta);
+    psi = getPsi(&foo2);
+    psi = convertRadToDeg(psi);
+    phi = getPhi(&foo2);
+    phi = convertRadToDeg(phi);
+    printf("Theta:%f  Psi:%f  Phi:%f\n", theta, psi, phi); //Theta:-5.727654  Psi:-33.542721  Phi:-11.460480
     return 0;
 }
+
