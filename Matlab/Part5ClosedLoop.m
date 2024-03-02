881af31b4faeed34fb2e92ca1c2c7d5a1ee69ffe
aasivaku <aasivaku@ucsc.edu>
2024-03-01 16:40:11 -0800

    part 5 acc 25

--- Matlab/Part5ClosedLoop.m
+++ Matlab/Part5ClosedLoop.m
@@ -20,10 +20,10 @@ gravity = 9.81;       % in meters per second squared
 nT = 23194.3/1000;           % nanoTeslas from microTeslas
 
 % Inertial gravity vector points opposite to the NED position vector
-accelInertial = [gravity; 0; 0];
+accelInertial = [0; 0; gravity];
 
 % Define a unit vector pointing in the direction of magnetic north
-magInertial = [0; 0; nT];
+magInertial = [nT; 0; 0];
 
 % Step 2: Extract true Euler angles and bias
 yaw_true = deg2rad(Eul_true(:, 1));
@@ -52,14 +52,18 @@ R_0 = [1, 0, 0;
 pitches =[];
 yaws = [];
 rolls = [];
-Bminus = [yaw_bias ; pitch_bias; roll_bias];
-% Bminus = [0; 0; 0];
+% Bminus = [yaw_bias ; pitch_bias; roll_bias];
+Bminus = [0; 0; 0];
 for i = 1:length(pitch_true)
-    
+    if i==1164
+        % disp(i)
+        % disp(wGyro(i,:))
+        disp(Rk_1)
+    end
     if i==1
-        [Rk_1, g_bias] = IntegrateClosedLoop(R_0, Bminus, deg2rad(wGyro(i, :)./131), Mag(i,:), Acc(i,:), magInertial, accelInertial, dT);
+        [Rk_1, g_bias] = IntegrateClosedLoop(R_0, Bminus, deg2rad(wGyro(i, :)'./131), Mag(i,:)', Acc(i,:)', magInertial, accelInertial, dT);
     else
-        [Rk_1, g_bias]= IntegrateClosedLoop(Rk_1, g_bias, deg2rad(wGyro(i, :)./131), Mag(i,:), Acc(i,:), magInertial, accelInertial, dT);
+        [Rk_1, g_bias]= IntegrateClosedLoop(Rk_1, g_bias, deg2rad(wGyro(i, :)'./131), Mag(i,:)', Acc(i,:)', magInertial, accelInertial, dT);
     end
     % Step 5: Extract Euler angles from integrated DCMs
     pitch = -asin(Rk_1(1, 3));
@@ -70,7 +74,8 @@ for i = 1:length(pitch_true)
     rolls = [rolls; rad2deg(roll)];
 end
 
-PrettyPlotAttitudeData(dT,Acc,Mag,wGyro,Eul_true)
+%PrettyPlotAttitudeData(dT,Acc,Mag,wGyro,Eul_true)
+ypr=[yaws, pitches, rolls];
 
 subplot(3, 1, 1);
 plot(rad2deg(yaw_true), 'b');
